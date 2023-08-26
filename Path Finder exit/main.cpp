b1f0f4642be4ebe582d9a6fd43fc0051e60c9275
militrik <militrik@gmail.com>
2023-08-26 20:51:53 +0300

    path_finder

--- Path Finder exit/main.cpp
+++ Path Finder exit/main.cpp
@@ -1,51 +1,68 @@
 #include <iostream>
 
 #include <string>
-#include <vector>
-#include <sstream>
 #include <algorithm>
+#include <list>
 
 using namespace std;
 
-enum Dirs {
-    UP, RIGHT, DOWN, LEFT
-} dir = RIGHT;
-
-
-bool step(std::string::iterator *ptr, const int *hStep, const int *vStep, string *maze,
-          Dirs dirs) {
-    if (*ptr - *vStep > maze->begin() && *(*ptr - *vStep) != 'W' && dir != static_cast<Dirs>((dir + 2) % 4)) {
-        *ptr -= *vStep;
-        //return step(&ptr, &hStep, &vStep, &maze, dir);
-    } else if (*(*ptr + *hStep) != '\n' && *(*ptr + *hStep) != 'W' && dir != static_cast<Dirs>((dir + 2) % 4)) {
-        *ptr += *hStep;
-        dir = static_cast<Dirs>((dir + 2) % 4);
-    } else if (*ptr + *vStep < maze->end() && *(*ptr + *vStep) != 'W' && dir != static_cast<Dirs>((dir + 2) % 4)) {
-        *ptr += *vStep;
-        dir = static_cast<Dirs>((dir + 2) % 4);
-    } else if (*(*ptr - *hStep) != '\n' && *(*ptr + *hStep) != 'W' && *ptr != maze->begin() &&
-               dir != static_cast<Dirs>((dir + 2) % 4)) {
-        *ptr -= *hStep;
-        dir = static_cast<Dirs>((dir + 2) % 4);
+void printMaze(std::string::iterator ptr, const std::string *maze) {
+    for (std::string::const_iterator it = maze->begin(); it != maze->end(); ++it) {
+        if (it == ptr) {
+            std::cout << 'x';
+        } else {
+            std::cout << *it;
+        }
     }
-    return false;
+    std::cout << std::endl << std::endl;
 }
 
 bool path_finder(string maze) {
     const int n = std::distance(maze.begin(), std::find(maze.begin(), maze.end(), '\n'));
-    const int maxPath = n * n + 1;
-    const int minPath = n;
     const int vStep = n + 1, hStep = 1;
-    std::string::iterator ptr = maze.begin();
+    std::list<std::string::iterator> list = {maze.begin()};
+    //*list.back() = 'W';
 
-    return step(&ptr, &hStep, &vStep, &maze, dir);
+    while (list.back() + 1 != maze.end()) {
+
+        std::string::iterator pos = list.back();
+        auto listPtr = std::prev(list.end());;
+        printMaze(pos, &maze);
+        if (*(pos + vStep) == '.') {//down
+            list.push_back(pos + vStep);
+        }
+        if (*(pos + hStep) == '.') {//right
+            list.push_back(pos + hStep);
+        }
+        if (*(pos - vStep) == '.') {// up
+            list.push_back(pos - vStep);
+        }
+        if (*(pos - hStep) == '.') {//left
+            list.push_back(pos - hStep);
+        }
+
+        maze[std::distance(maze.begin(), pos)] = 'W';
+        if (list.empty())
+            return false;
+        else
+            list.erase(listPtr);
+
+    }
+    return true;
 }
 
 int main() {
+    std::cout << path_finder("....\n..W.\nW..W\n.W..") << true << std::endl;
     std::cout << path_finder(".W.\n.W.\n...") << true << std::endl;
     std::cout << path_finder(".W.\n.W.\nW..") << false << std::endl;
     std::cout << path_finder("......\n......\n......\n......\n......\n......") << true << std::endl;
     std::cout << path_finder("......\n......\n......\n......\n.....W\n....W.") << false << std::endl;
+    std::cout << path_finder(
+            "....W.....\n....W.W.WW\n..WWWWW...\n..W...W.W.\n..W.W.W.W.\n..W.W.W.W.\n..W.W.W.W.\n..W.W.W.W.\n..W.W.W.W.\n....W...W.")
+              << true << std::endl;
+    std::cout << path_finder(
+            "....W.....\n....W.W.WW\n..WWWWW...\n..W...W.W.\n..W.W.W.W.\n..W.W.W.W.\n..W.W.W.W.\n..W.W.W.W.\n..W.W.W.WW\n....W...W.")
+              << false << std::endl;
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
