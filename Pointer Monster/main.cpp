a7d4c3ee9cea294cd83f16134a22eff22d820d5f
militrik <militrik@gmail.com>
2023-09-15 23:11:29 +0300

    pointer_monster

---
+++ Pointer Monster/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <iostream>
+
+char a[4][7] = {"Common", "Point", "Boost", "Better"};
+char (*b[4])[7] = {a + 3, a + 1, a, a + 2};
+
+char (*(*C())[4])[7] {
+    return &b;
+}
+
+char (**d())[7] {
+    return C()[1] - 3;
+}
+
+char buf[256];
+
+char *pointer_monster(char (**(*f)())[7]) {
+    int len;
+    len = sprintf(buf, "%s", *f()[0]);
+    len += sprintf(buf + len, "%s ", *((**f)() - 1)[0] + 4);
+    len += sprintf(buf + len, "%s", (*f())[0] - 4);
+    len += sprintf(buf + len, "%s", f()[1][2] + 3);
+    len += sprintf(buf + len, "%s", *((**f)() - 1)[0] + 4);
+    return buf;
+}
+
+
+int main() {
+
+    std::cout << pointer_monster(d) << std::endl;
+
+    return 0;
+}
